9853a4626c0ca92aadcd030501e36ea0e17240bb
Muhammad fahad umer <eng.fahadumer@gmail.com>
2020-05-06 00:46:20 +0500

    stickler errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,20 +18,21 @@ def bubble_sort(sorted_array)
   p sorted_array
 end
 
-def bubble_sort_by (sorted_array)
+def bubble_sort_by(sorted_array)
   check = true
   while check
     check = false
-    for i in (0...sorted_array.length - 1) do
-      next unless yield(sorted_array[i], sorted_array[i + 1]) > 0
-        sorted_array[i] , sorted_array[i + 1] = sorted_array[i + 1] , sorted_array[i]
-        check = true
+    (0...sorted_array.length - 1).each do |i|
+
+      next unless yield(sorted_array[i], sorted_array[i + 1]).positive?
+      sorted_array[i], sorted_array[i + 1] = sorted_array[i + 1], sorted_array[i]
+      check = true
     end
   end
   p sorted_array
 end
 
-bubble_sort_by(%w[hi hello hey hi]) do |left,right|
+bubble_sort_by(%w[hi hello hey hi]) do |left, right|
   left.length - right.length
 end
 bubble_sort([4, 3, 78, 2, 0, 2])
