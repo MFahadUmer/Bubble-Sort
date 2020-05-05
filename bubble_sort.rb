97db133c9998c925639f0250d86e94ac2ef3394f
Muhammad fahad umer <eng.fahadumer@gmail.com>
2020-05-06 00:42:24 +0500

    yield added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,27 +18,22 @@ def bubble_sort(sorted_array)
   p sorted_array
 end
 
-def bubble_sort_by(sorted_array)
+def bubble_sort_by (sorted_array)
   check = true
   while check
-    counter = 0
-    sorted_array.each_with_index do |_elements, i|
-      if i <= sorted_array.length - 2
-        if (sorted_array[i].length <=> sorted_array[i + 1].length) == 1
-          temp = sorted_array[i]
-          sorted_array[i] = sorted_array[i + 1]
-          sorted_array[i + 1] = temp
-          counter += 1
-        elsif counter.zero? && i == sorted_array.length - 2
-          check = false
-        end
-      end
+    check = false
+    for i in (0...sorted_array.length - 1) do
+      next unless yield(sorted_array[i], sorted_array[i + 1]) > 0
+        sorted_array[i] , sorted_array[i + 1] = sorted_array[i + 1] , sorted_array[i]
+        check = true
     end
   end
   p sorted_array
 end
 
-bubble_sort_by(%w[hello hi baby hey hi])
+bubble_sort_by(%w[hi hello hey hi]) do |left,right|
+  left.length - right.length
+end
 bubble_sort([4, 3, 78, 2, 0, 2])
 bubble_sort([6, 5, 3, 1, 8, 7, 2, 4])
 bubble_sort([7, 2, 8, 0, 4, 1, 3])
